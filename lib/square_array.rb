67c6afc71d0242541ee0d0dd7b6b939d1aa0beb6
allison cortez <allizetorc@gmail.com>
2019-09-27 21:27:19 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,6 @@
 def square_array(array)
   counter=0
-  while counter << array.length do
-    square_array(num**2)
-    
-    new numbers=(num**2)
+  while counter < array.length do
+    square_array(counter**2)
+  
 end
